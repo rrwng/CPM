704a14187fd1d0eb1c9665761a2a9c1584409aa1
rrwng <57116839+rrwng@users.noreply.github.com>
2021-06-10 15:46:55 -0400

    Add files via upload

--- cdist.m
+++ cdist.m
@@ -8,17 +8,14 @@ function Dst = cdist(D,no_dims,compel_force)
      nscales = 7; % number of scales for dimension estimation
      [dim1,r] = compute_dim(sqrt(D),nscales,0.01);
      if compel_force == 1
-         dim1(2) = 1.5*dim1(2);
+         dim1(2) = 2*dim1(2);
      end
      max_dim = 10*(no_dims-1);   % set an upper bound on the estimated dimension to avoid divergence when the data dimension is too big
      ind = (dim1<max_dim);
      dim = ones(size(dim1))*max_dim;
      dim(ind) = dim1(ind);
      dim(dim1<no_dims) = no_dims; %the lower bound on the estimated dimension is set to the mapped-to dimension
-     for i=1:nscales-1
-         dim(i+1)=max(dim(i),dim(i+1));
-     end
-     
+  
      r1 = prctile(sort(sqrt(D(:))),1); % a small postive number preventing dividing by 0 when defining the probability
      [~, I] = sort(D(:),'ascend');
      Dst = zeros(n,n);
@@ -28,7 +25,8 @@ function Dst = cdist(D,no_dims,compel_force)
           Dst(i,j) = (r1^2+(D(i,j)))^((curr_dim)/(no_dims));   % defined the new distance
          end
      end
- 
+       Dst(I) =  make_mono(sqrt(Dst(I)),sqrt(D(I)+r1^2)); 
+
      Dst = sqrt(Dst);
 end
 function d = find_dim(r,dim,a)
@@ -43,4 +41,11 @@ while i<= length(r)
     end
 end
 end
-
+function x=make_mono(x,y)
+d = max(x(1)-y(1),0);
+for i = 2:length(x)
+    x(i) = max([x(i),x(i-1),y(i)+d]);
+    d = max(d,max(x(i)-y(i),0));
+end
+x = x.^2;
+end
